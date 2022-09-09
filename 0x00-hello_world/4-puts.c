39d74b42800a9b91c6644f6d1365230e669dd669
TOCVIC <vickytochi6882@gmail.com>
2022-09-08 17:43:44 -0700

    4-pits.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,3 @@
-/* 
-  * File: 4-puts.c 
-  * Auth: Brennan D Baraban <375@holbertonschool.com> 
-  */ 
-  
  #include <stdio.h> 
   
  /** 
@@ -11,8 +6,8 @@
   * 
   * Return: Always 0. 
   */ 
- int main(void) 
- { 
+int main(void)
+{ 
          puts("\"Programming is like building a multilingual puzzle"); 
   
          return (0); 

2e71d75a558ea8981416a4192fe46875a13fc994
TOCVIC <vickytochi6882@gmail.com>
2022-09-13 23:04:40 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,21 @@
-#include "main.h"
+#include "main.h"
 
 /**
- * print_alphabet_x10 - prints alphabets 10
- *
- * Description: prints 10 times
- * Return: Always(0).
+* print_alphabet_x10 - prints alphabets 10
+*
+* Description: prints 10 times
+* Return: Always(0).
 */
 
-void print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
-	int alphabet_count = 0;
+	int alphabet_count = 0;
 
-	while (alphabet_count < 10)
+	while (alphabet_count < 10)
 	{
-		char ch = 'a';
+		char ch = 'a';
 
-		while (ch <= 'z')
+		while (ch <= 'z')
 		{
 			_putchar(ch);
 			ch++;
@@ -24,3 +24,4 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 	}
 }
+

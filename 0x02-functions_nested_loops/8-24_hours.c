e5ddc15394ff864403cf9e51f0c894e73776519e
TOCVIC <vickytochi6882@gmail.com>
2022-09-13 20:55:10 -0700

    j

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,51 +1,29 @@
 #include "main.h"
 
-
-
 /**
-
- * jack_bauer - prints time
-
- *
-
- * Description: prints a list of time
-
- * Return: Always 0.
-
- */
-
-
+* jack_bauer - prints time
+* Description: prints a list of time
+* Return: Always 0.
+*/
 
 void jack_bauer(void)
-  
 {
-  
-  int i, j;
-  
+	int i, j;
 
-  
-  for (i = 0; i < 24; i++)
-    
-    {
-      
-      for (j = 0; j < 60; j++)
-	
+	for (i = 0; i < 24; i++)
+
 	{
-	  
-	  _putchar(i / 10 + 48);
-	  
-	  _putchar(i % 10 + 48);
-	  
-	  _putchar(':');
-	  
-	  _putchar(j / 10 + 48);
-	  
-	  _putchar(j % 10 + 48);
-	  
-	  _putchar('\n');
-	  
+		for (j = 0; j < 60; j++)
+		{
+
+			_putchar(i / 10 + 48);
+			_putchar(i % 10 + 48);
+			_putchar(':');
+			_putchar(j / 10 + 48)
+			_putchar(j % 10 + 48);
+			_putchar('\n');
+		}
+
 	}
-      
-    }
-  
+
 }

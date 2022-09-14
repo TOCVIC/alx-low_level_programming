bae82746c9cf8d111e9d509dbd9b26ae5a41f383
TOCVIC <vickytochi6882@gmail.com>
2022-09-13 17:39:58 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,22 @@
-#include "main.h"
+#include "main.h" 
 /**
- * main - entry point
+ * main - prints Putchar
  *
- * Description: Prints Main with _putchar
+ * Description: prints _putchar
  *
- * Return: always 0
- */
-
-int _putchar(void);
+ * Return: 0 (Success)
+ * /
+int main(void)
 {
-
-	char c[] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-
-		_putchar(c[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-	return (0);
+
+	return (0);
 }

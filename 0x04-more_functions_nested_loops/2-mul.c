87853eb10b9a8cf470baa0f0fc4ff476db2a03c1
TOCVIC <vickytochi6882@gmail.com>
2022-09-15 18:17:54 -0700

    2-task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: multiplying
+ * @b: multiplier
+ *
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int a;
+	int b;
+	{
+		printf("%d", a * b);
+	}
+	return (0)
+}

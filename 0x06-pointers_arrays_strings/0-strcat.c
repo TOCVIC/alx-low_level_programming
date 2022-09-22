43991b76c7258b514627b44498937047481db526
TOCVIC <vickytochi6882@gmail.com>
2022-09-22 08:35:24 -0700

    0 task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,7 +12,7 @@
 char *_strcat(char *dest, char *src)
 {
 	char destination(98) = "Hello ";
-	char source(1024) = "World! ";
+	char source(1024) = "World!\n";
 
 	printf("concatenated string: %s\n", strcat(destination, source));
 

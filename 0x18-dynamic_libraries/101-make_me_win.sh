302f77d4eb48788cc7f685b03a0ade6cc4fe0c71
TOCVIC <86574097+TOCVIC@users.noreply.github.com>
2022-12-13 01:22:05 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Tocvic/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -O /tmp/win.so https://github.com/Tocvic/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.sh
+export LD_PRELOAD=/tmp/win.sh
 
 
